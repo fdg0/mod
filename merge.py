126b55a937155661ab1c6f0af29ff2a26b0f8fb4
fede <fdg0@outlook.com>
2022-11-18 17:54:53 -0300

    two

--- merge.py
+++ merge.py
@@ -1,30 +1,24 @@
 import shutil
 import os
 
-path = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340'
+# Source path 
+src = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340/'
+ 
+# Destination path 
+dest = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340/multiplayer_synchronized/'
 
-def make_new_folder(folder_name, parent_folder):
-    path = os.path.join(parent_folder, folder_name)
-    try: 
-        mode = 0o777
-        os.mkdir(path, mode) 
-    except OSError as error: 
-        print(error)
-
-current_folder = path
-list_dir = os.listdir(path)
+list_dir = os.listdir(src)
 list_dir.remove('.git')
 list_dir.remove('.gitattributes')
 list_dir.remove('merge.py')
-content_list = {}
-for index, val in enumerate(list_dir):
-    path = os.path.join(current_folder, val)
-    content_list[ list_dir[index] ] = os.listdir(path)
-merge_folder = "merge_folder"
-merge_folder_path = os.path.join(current_folder, merge_folder) 
-make_new_folder(merge_folder, current_folder)
-for sub_dir in content_list:
-    for contents in content_list[sub_dir]:
-        path_to_content = sub_dir + "/" + contents  
-        dir_to_move = os.path.join(current_folder, path_to_content )
-        shutil.move(dir_to_move, merge_folder_path)
+list_dir.remove('multiplayer_synchronized')
+for sub_dir in list_dir:
+    sub_dir_2 = src+sub_dir
+    try: 
+        shutil.copytree(sub_dir_2, dest,dirs_exist_ok=True)
+    except OSError as error: 
+        print(error)
+    
+
+
+            
