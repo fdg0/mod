3acb53eb6e5aa8d6c072c7f8dcc758544125c129
fede <fdg0@outlook.com>
2022-11-22 09:09:17 -0300

    new mod

--- merge.py
+++ merge.py
@@ -5,13 +5,13 @@ import os
 src = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340/'
  
 # Destination path 
-dest = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340/multiplayer_synchronized/'
+dest = 'C:/Program Files (x86)/Steam/steamapps/workshop/content/529340/multiplayersynchronized/'
 
 list_dir = os.listdir(src)
 list_dir.remove('.git')
 list_dir.remove('.gitattributes')
 list_dir.remove('merge.py')
-list_dir.remove('multiplayer_synchronized')
+list_dir.remove('multiplayersynchronized')
 list_dir.remove('.gitignore')
 
 for sub_dir in list_dir:
